3be077dfb6fd8d47e4806ff0478c43187978922c
ozwonek <azwonek@student.agh.edu.pl>
2025-10-04 23:24:38 +0200

    Profile changes

--- client/untitled-ui/src/pages/profil/profile.tsx
+++ client/untitled-ui/src/pages/profil/profile.tsx
@@ -1,26 +1,31 @@
+import { ArrowLeft } from "@untitledui/icons";
+import { Badge } from "@/components/base/badges/badges";
+import { Button } from "@/components/base/buttons/button";
+import { ProfileHeader } from "./profile-header";
 
- import { Badge } from "@/components/base/badges/badges";
- import { ProfileHeader } from "./profile-header";
- import { ArrowLeft } from "@untitledui/icons";
- import { Button } from "@/components/base/buttons/button";
 export const Profile = () => {
-
     const onClick = () => {
         window.history.back();
-    }
+    };
 
-    return (
-        
-        <div className="flex max-w-89 justify-between items-center p-4">
-            <Button size="sm" color="link-gray" onClick={onClick}>
-                <ArrowLeft/>
-            </Button>
-            <Badge type="pill-color" color="gray" size="lg">
-                My profile
-            </Badge>
-            <ProfileHeader>
-            </ProfileHeader>
-        
-        </div>
-    );
+   return (
+  <div className="flex max-w-89 flex-col gap-6 p-4">
+    <div className="relative flex items-center justify-center">
+      <Button
+        size="sm"
+        color="link-gray"
+        onClick={onClick}
+        className="absolute left-0"
+      >
+        <ArrowLeft />
+      </Button>
+      <Badge type="pill-color" color="gray" size="lg">
+        My profile
+      </Badge>
+    </div>
+    <div className="flex justify-start w-full">
+      <ProfileHeader />
+    </div>
+  </div>
+);
 };
