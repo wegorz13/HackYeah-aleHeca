0006035315e91ad69edeb216bf75e530f33992e7
Kacper Kopiec <kkopiec@student.agh.edu.pl>
2025-10-05 01:52:28 +0200

    Profile images

--- client/untitled-ui/src/pages/profil/profile-header.tsx
+++ client/untitled-ui/src/pages/profil/profile-header.tsx
@@ -1,30 +1,22 @@
-
- import { useState, useEffect } from "react";
- import { Badge } from "@/components/base/badges/badges";
+import { useEffect, useState } from "react";
 
 export const ProfileHeader = () => {
     const userId = 2;
-    
- 
-     const [user, setUser] = useState([]);
-    
-        useEffect(() => {
-            fetch(`http://localhost:3000/user/${userId}`)
-                .then((res) => res.json())
-                .then((data) => setUser(data))
-                .catch((err) => console.error(err));
-        }, [userId]);
-    
-        console.log(user);
+
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/user/${userId}`)
+            .then((res) => res.json())
+            .then((data) => setUser(data))
+            .catch((err) => console.error(err));
+    }, [userId]);
+
+    console.log(user);
     return (
-        
-        <div className="flex flex-row items-center gap-3  ">
-            <div className="text-lg font-bold">
-            {user.name} 
-                </div>
-            <div className="text-gray-500 text-md ml-2">
-                {user.age}
-            </div>
+        <div className="flex flex-row items-center gap-3">
+            <div className="text-lg font-bold">{user.name}</div>
+            <div className="text-md text-gray-500">{user.age}</div>
         </div>
     );
 };
