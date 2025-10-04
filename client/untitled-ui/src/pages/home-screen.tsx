600c3234f71ec7d892a02f09b7d2e33b38bbfe96
ozwonek <azwonek@student.agh.edu.pl>
2025-10-04 17:42:34 +0200

    frontend setup

---
+++ client/untitled-ui/src/pages/home-screen.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/base/buttons/button";
+
+export const HomeScreen = () => {
+
+    return (
+        <div className="flex">
+           
+            <div>
+                <Button color="primary" size="sm">Button sm</Button>
+            </div>
+            
+        </div>
+    );
+};
