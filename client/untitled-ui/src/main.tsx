5a790ecbd37b944895c7f250d90cfcb563da6c7c
nczajak <nczajak@student.agh.edu.pl>
2025-10-05 03:32:11 +0200

    exploring page

--- client/untitled-ui/src/main.tsx
+++ client/untitled-ui/src/main.tsx
@@ -8,12 +8,6 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import "@/styles/globals.css";
 import {Explorer} from "@/pages/explorer.tsx";
 
-const profiles = [
-    { name: "Ola", age: 23, city: "Kraków", rating: 8 },
-    { name: "Filip", age: 29, city: "Warszawa", rating: 6 },
-    { name: "Natalia", age: 21, city: "Gdańsk", rating: 9 },
-];
-
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <ThemeProvider>
@@ -21,7 +15,7 @@ createRoot(document.getElementById("root")!).render(
                 <RouteProvider>
                     <Routes>
                         <Route path="/" element={<HomeScreen />} />\
-                        <Route path="/explorer" element={<Explorer city="ola" profiles={profiles}/>} />
+                        <Route path="/explorer" element={<Explorer city="City 4"/>} />
                         <Route path="/" element={<HomeScreen />} />
                         <Route path="/profile" element={<Profile />} />
                     </Routes>
